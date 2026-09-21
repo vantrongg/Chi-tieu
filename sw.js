b3a53443dce0bd80ae241ccfe28f40817c95b83e
vantrongg <45504591+vantrongg@users.noreply.github.com>
2026-09-21 16:18:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chi-tieu-v4";
+const CACHE = "chi-tieu-v7";
 
 const FILES = [
   "./",
@@ -8,58 +8,33 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-    caches
-      .open(CACHE)
-      .then(cache =>
-        cache.addAll(FILES)
-      )
+    caches.open(CACHE).then(cache =>
+      cache.addAll(FILES)
+    )
   );
 
   self.skipWaiting();
-
 });
 
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys().then(keys =>
-
       Promise.all(
-
         keys
-          .filter(key =>
-            key !== CACHE
-          )
-          .map(key =>
-            caches.delete(key)
-          )
-
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
       )
-
     )
-
   );
 
   self.clients.claim();
-
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
-    caches
-      .match(event.request)
-      .then(response =>
-        response ||
-        fetch(event.request)
-      )
-
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
-
 });
