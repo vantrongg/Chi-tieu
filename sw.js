0837feb447028284a7f5a29ffa3f1c2453342781
vantrongg <45504591+vantrongg@users.noreply.github.com>
2026-09-21 15:46:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,65 @@
-const CACHE = "chi-tieu-v3";
+const CACHE = "chi-tieu-v4";
 
 const FILES = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-1024.PNG"
+  "./icon-512.PNG"
 ];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache =>
+        cache.addAll(FILES)
+      )
+  );
+
+  self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+          .filter(key =>
+            key !== CACHE
+          )
+          .map(key =>
+            caches.delete(key)
+          )
+
+      )
+
+    )
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches
+      .match(event.request)
+      .then(response =>
+        response ||
+        fetch(event.request)
+      )
+
+  );
+
+});
