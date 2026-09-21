c43abf483383e436bd3a0ffab438c7896c99d04d
vantrongg <45504591+vantrongg@users.noreply.github.com>
2026-09-21 11:21:30 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = "chi-tieu-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response =>
+        response ||
+        fetch(event.request)
+      )
+  );
+});
